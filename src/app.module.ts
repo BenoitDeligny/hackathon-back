d615dddeb5221a466605eb407e1a8af650070e48
Benoit Deligny <benoit.deligny@manomano.com>
2022-08-03 00:44:30 +0200

    Create controller (with first simple test) and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PatientController } from './patient/controller/PatientController';
+import { PatientService } from './patient/service/PatientService';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [PatientController],
+  providers: [PatientService],
 })
 export class AppModule { }
