eb59e7a42200170f962155b44fe88d60893fea38
Benoit Deligny <benoit.deligny@manomano.com>
2022-08-07 17:40:31 +0200

    Rrefacto module and add validation on DTO

--- src/patient/model/PatientDto.ts
+++ src/patient/model/PatientDto.ts
@@ -1,16 +1,26 @@
+import { IsOptional, IsString } from "class-validator";
+
 export class PatientDto {
-    public id: number;
+    public id: number; // TODO: remove it when real DB will be set up
+
+    @IsString()
     public lastname: string;
+
+    @IsString()
     public firstname: string;
+
+    @IsString()
     public address: string;
+
+    @IsOptional()
+    @IsString()
     public email: string;
 
-    // TODO: add validator for non-nullable field
     constructor(id: number, lastname: string, firstname: string, address: string, email: string) {
         this.id = id;
         this.lastname = lastname;
         this.firstname = firstname;
         this.address = address;
-        this.email = email // could be null
+        this.email = email
     }
 }
