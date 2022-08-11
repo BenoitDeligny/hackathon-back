2489ef86d97de4cfa3327922dcd69d7914de6778
Benoit Deligny <benoit.deligny@manomano.com>
2022-08-11 18:51:00 +0200

    Rrefacto module and add validation on DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PatientController } from './patient/controller/Patient.controller';
-import { PatientService } from './patient/service/Patient.service';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [],
-  controllers: [PatientController],
-  providers: [PatientService],
+  imports: [PatientModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
